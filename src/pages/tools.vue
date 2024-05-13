ff39cee49d0244eedcdfa5faba6a223899b8e667
wangyupu <wangyupu4513@gmail.com>
2024-05-13 22:14:01 +0800

    工具索引，递增版本

--- src/pages/tools.vue
+++ src/pages/tools.vue
@@ -3,9 +3,14 @@
 </script>
 
 <template>
-<h1>wangyupu 工具索引</h1>
+<div class="tool_index">
+    <h1>wangyupu 工具索引</h1>
+    <router-link to="/tool/nngweb">网易MC名称生成器</router-link>
+</div>
 </template>
 
 <style scoped>
-
+.tool_index {
+    text-align: center;
+}
 </style>
