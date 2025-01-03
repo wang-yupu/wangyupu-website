40ec8c7d3f38102dc90d98f0b49da4c3e51e76c7
wangyupu <wangyupu4513@gmail.com>
2025-01-03 13:34:37 +0800

    BUG: SHA只切分了7位，修改到8位

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ export const lastProdCommitTime = ref("????????")
 // 同步
 fetch("https://api.github.com/repos/wang-yupu/wangyupu-website/branches/main").then(async (resp)=>{
     const json = await resp.json()
-    lastProdCommitSHA.value = json.commit.sha.slice(0,7)
+    lastProdCommitSHA.value = json.commit.sha.slice(0,8)
     lastProdCommitTime.value = json.commit.commit.author.date
 })
 export default isPageLoading
