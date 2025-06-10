bb95a43118085b7ca100635005075e28fed1d8f3
wangyupu <wangyupu4513@gmail.com>
2025-06-11 00:26:00 +0800

    修改nng的一段文本

--- src/pages/tools/nngv/page.vue
+++ src/pages/tools/nngv/page.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref, watch, reactive } from 'vue'
-import words from './words.json'
-import { useClipboard } from '@vueuse/core'
+import { ref, watch, reactive } from 'vue';
+import words from './words.json';
+import { useClipboard } from '@vueuse/core';
 
-const name = ref("")
-const showModalAlert = ref(false)
-const { text, copy, copied, isSupported } = useClipboard({ name })
+const name = ref('');
+const showModalAlert = ref(false);
+const { text, copy, copied, isSupported } = useClipboard({ name });
 
 function choiceitem(array) {
     var randomIndex = Math.floor(Math.random() * array.length);
@@ -15,124 +15,119 @@ function choiceitem(array) {
 function randomName() {
     var method = choiceitem([1, 2, 3, 4, 5, 6, 7]);
     if (method == 1) {
-        return choiceitem(words.db["1"]) + choiceitem(words.db["2"]) + choiceitem(words.db["0"]);
+        return choiceitem(words.db['1']) + choiceitem(words.db['2']) + choiceitem(words.db['0']);
     }
     if (method == 2) {
-        return choiceitem(words.db["1"]) + choiceitem(words.db["3"]) + choiceitem(words.db["0"]);
+        return choiceitem(words.db['1']) + choiceitem(words.db['3']) + choiceitem(words.db['0']);
     }
     if (method == 3) {
-        return choiceitem(words.db["1"]) + choiceitem(words.db["3"]) + choiceitem(words.db["1"]);
+        return choiceitem(words.db['1']) + choiceitem(words.db['3']) + choiceitem(words.db['1']);
     }
     if (method == 4) {
-        return choiceitem(words.db["1"]) + choiceitem(words.db["5"]) + choiceitem(words.db["1"]);
+        return choiceitem(words.db['1']) + choiceitem(words.db['5']) + choiceitem(words.db['1']);
     }
     if (method == 5) {
-        return choiceitem(words.db["1"]) + choiceitem(words.db["0"]) + choiceitem(words.db["3"]);
+        return choiceitem(words.db['1']) + choiceitem(words.db['0']) + choiceitem(words.db['3']);
     }
     if (method == 6) {
-        return choiceitem(words.db["1"]) + choiceitem(words.db["0"]) + choiceitem(words.db["2"]);
+        return choiceitem(words.db['1']) + choiceitem(words.db['0']) + choiceitem(words.db['2']);
     }
     if (method == 7) {
-        return choiceitem(words.db["2"]) + "的" + choiceitem(words.db["1"]) + choiceitem(words.db["4"]);
+        return choiceitem(words.db['2']) + '的' + choiceitem(words.db['1']) + choiceitem(words.db['4']);
     }
-    return "生成错误";
+    return '生成错误';
 }
 
 function randomNameClick() {
-    name.value = randomName()
+    name.value = randomName();
 }
 
-const windowHeight = ref(100)
-const windowWidth = ref(100)
-const windowWidthLimit = ref(100)
+const windowHeight = ref(100);
+const windowWidth = ref(100);
+const windowWidthLimit = ref(100);
 function updateWH() {
     windowHeight.value = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     windowWidth.value = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    windowWidthLimit.value = windowWidth.value
+    windowWidthLimit.value = windowWidth.value;
     if (windowWidth.value > 800) {
-        windowWidthLimit.value = 800
+        windowWidthLimit.value = 800;
     }
 }
 window.addEventListener('resize', updateWH);
 
-updateWH()
+updateWH();
 
-const showAlert = ref(false)
-const showSensitiveAlert = ref(false)
-const showShortAlert = ref(false)
+const showAlert = ref(false);
+const showSensitiveAlert = ref(false);
+const showShortAlert = ref(false);
 watch(name, (newVal) => {
     if (name.value.length == 0) {
-        showAlert.value = true
-        showShortAlert.value = true
+        showAlert.value = true;
+        showShortAlert.value = true;
     } else {
-        showAlert.value = false
-        showShortAlert.value = false
+        showAlert.value = false;
+        showShortAlert.value = false;
     }
-})
+});
 
-randomNameClick()
+randomNameClick();
 
 // useAPI Animation border!
 // border-image: conic-gradient(#ff0000,#ffc400,#ffff00, #00ff00,#00ffff,#0000ff,#ff00ff,#ff0000) 1 1 /4px 4px;
-const gradientAngle = ref(0)
+const gradientAngle = ref(0);
 const useAPIElementStyle = reactive({
-    "border-image": `conic-gradient(from ${gradientAngle.value}deg,#ff0000,#ffc400,#ffff00, #00ff00,#00ffff,#0000ff,#ff00ff,#ff0000) 1 1 /4px 4px`
-})
+    'border-image': `conic-gradient(from ${gradientAngle.value}deg,#ff0000,#ffc400,#ffff00, #00ff00,#00ffff,#0000ff,#ff00ff,#ff0000) 1 1 /4px 4px`,
+});
 
-function updateAngle(){
+function updateAngle() {
     if (gradientAngle.value == 360) {
-        gradientAngle.value = 0
+        gradientAngle.value = 0;
     } else {
-        gradientAngle.value += 3
+        gradientAngle.value += 3;
     }
     // 触发样式更新
-    useAPIElementStyle["border-image"] = `conic-gradient(from ${gradientAngle.value}deg,#ff0000,#ffc400,#ffff00, #00ff00,#00ffff,#0000ff,#ff00ff,#ff0000) 1 1 /4px 4px`
-    requestAnimationFrame(updateAngle)
+    useAPIElementStyle['border-image'] = `conic-gradient(from ${gradientAngle.value}deg,#ff0000,#ffc400,#ffff00, #00ff00,#00ffff,#0000ff,#ff00ff,#ff0000) 1 1 /4px 4px`;
+    requestAnimationFrame(updateAngle);
 }
 
-requestAnimationFrame(updateAngle)
-
+requestAnimationFrame(updateAngle);
 </script>
 
 <template>
     <div class="nng">
         <div class="content">
             <div class="toastContainer">
-                <img src="/src/assets/nng/toast.png" class="toast" :width="windowWidthLimit * 0.6">
+                <img src="/src/assets/nng/toast.png" class="toast" :width="windowWidthLimit * 0.6" />
             </div>
             <div class="signContainer">
                 <img src="/src/assets/nng/sign_with_panda.png" :height="windowHeight * 0.6" class="sign" />
-                <button class="randomButton" @click="randomNameClick">&nbsp;&nbsp;<br>&nbsp;</button>
-                <input class="nameInput" maxlength="12" v-model="name" placeholder="遵纪守法文明用语">
+                <button class="randomButton" @click="randomNameClick">&nbsp;&nbsp;<br />&nbsp;</button>
+                <input class="nameInput" maxlength="12" v-model="name" placeholder="遵纪守法文明用语" />
                 <div class="alertContainer" v-if="showAlert">
-                    <img class="alert" :width="windowWidthLimit * 0.1" src="/src/assets/nng/alert.png">
+                    <img class="alert" :width="windowWidthLimit * 0.1" src="/src/assets/nng/alert.png" />
                     <span class="alertText" v-if="showSensitiveAlert">包含敏感词</span>
                     <span class="alertTextShort" v-if="showShortAlert">昵称太短</span>
                 </div>
                 <Transition name="modalAlert">
                     <div class="modalAlertContainer" v-if="showModalAlert">
-                        <img class="alertBG" src="/src/assets/nng/sensitiveAlertBG.png"
-                            :width="windowWidthLimit * 0.55">
+                        <img class="alertBG" src="/src/assets/nng/sensitiveAlertBG.png" :width="windowWidthLimit * 0.55" />
                         <span>含有敏感词</span>
                     </div>
                 </Transition>
             </div>
             <div class="footer">
-                <span>打开音乐体验更佳</span><br>
-                <iframe class="mp" :width="windowWidth * 0.9" height=50
-                    src="//music.163.com/outchain/player?type=2&id=4010201&height=32"></iframe>
+                <span>打开音乐体验更佳</span><br />
+                <iframe class="mp" :width="windowWidth * 0.9" height="50" src="//music.163.com/outchain/player?type=2&id=4010201&height=32"></iframe>
                 <div class="infomations">
                     <div>
-                        <img src="/src/assets/nng/info.png" height="50">
+                        <img src="/src/assets/nng/info.png" height="50" />
                         <div>
                             <span>词库从网易MC(安卓)中提取</span>
-                            <!-- <span>违禁词检测使用了<a href="https://github.com/hotoo/pinyin" class="nocolor_link">hotoo/pinyin</a></span>
-                        <span>违禁词库版本 Example v0["1"]</span> -->
-                            <span>违禁词检测正在实现</span>
+                            <span>违禁词检测未实现（欢迎PR）</span>
                         </div>
                     </div>
                     <div>
-                        <img src="/src/assets/nng/feedback.png" height="50">
+                        <img src="/src/assets/nng/feedback.png" height="50" />
                         <div>
                             <span>有问题/建议？</span>
                             <span>在<router-link to="/contact" class="nocolor_link index_link">此处</router-link>联系我</span>
@@ -140,7 +135,7 @@ requestAnimationFrame(updateAngle)
                         </div>
                     </div>
                     <div class="useAPI" :style="useAPIElementStyle">
-                        <img src="/src/assets/nng/cloud.png" height="50">
+                        <img src="/src/assets/nng/cloud.png" height="50" />
                         <div>
                             <span>调用API</span>
                             <span>点<a class="nocolor_link" href="https://wapi.wangyupu.com/api/nng">这里</a>查看</span>
@@ -156,7 +151,7 @@ requestAnimationFrame(updateAngle)
 .nng {
     /* font-family: Unifont,system-ui, Avenir, Helvetica, Arial, sans-serif; */
     text-align: center;
-    background-image: url("/src/assets/nng/background.png");
+    background-image: url('/src/assets/nng/background.png');
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
@@ -183,7 +178,7 @@ requestAnimationFrame(updateAngle)
 .toastContainer::before {
     position: absolute;
     top: 17vh;
-    content: "探索你的世界前，给自己起个名字吧";
+    content: '探索你的世界前，给自己起个名字吧';
     color: white;
     border-radius: 5px;
     left: 50%;
@@ -194,7 +189,7 @@ requestAnimationFrame(updateAngle)
 .toastContainer::after {
     position: absolute;
     top: 21vh;
-    content: "稍后也可以在【设置】中更改名称";
+    content: '稍后也可以在【设置】中更改名称';
     color: white;
     border-radius: 5px;
     left: 50%;
@@ -211,7 +206,7 @@ requestAnimationFrame(updateAngle)
 
     .toastContainer::after {
         font-size: 2vw;
-        top: 18vh
+        top: 18vh;
     }
 }
 
@@ -308,7 +303,7 @@ requestAnimationFrame(updateAngle)
     background-color: rgba(104, 61, 31, 0.9);
 }
 
-.modalAlertContainer>span {
+.modalAlertContainer > span {
     position: absolute;
     left: 43%;
     top: 31%;
@@ -320,13 +315,7 @@ requestAnimationFrame(updateAngle)
 }
 
 .mp {
-    background: conic-gradient(
-      #ff00ff 0.25turn,
-      #000 0.25turn 0.5turn,
-      #ff00ff 0.5turn 0.75turn,
-      #000 0.75turn
-    )
-    top left / 15px 15px repeat;
+    background: conic-gradient(#ff00ff 0.25turn, #000 0.25turn 0.5turn, #ff00ff 0.5turn 0.75turn, #000 0.75turn) top left / 15px 15px repeat;
     border: 1px solid #00ffff;
 }
 
@@ -346,7 +335,7 @@ requestAnimationFrame(updateAngle)
     align-items: center;
 }
 
-.infomations>div>div {
+.infomations > div > div {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
